674cf30f13eabd40a7f976869cced00eba755803
Stuart Gale <bishboria@gmail.com>
2013-11-17 12:20:50 +0000

    Add rbenv to install script

--- install.sh
+++ install.sh
@@ -5,7 +5,13 @@ ln -s ~/.dotfiles/gitignore ~/.gitignore
 
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
-ln -s ~/.dotfiles/.emacs ~/.emacs
+ln -s ~/.dotfiles/emacs ~/.emacs
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 
+# begin work stuff
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+# end   work stuff
+
+git clone git://github.com/gmarik/vundle.git ~/.dotfiles/vundle.git
 vim +BundleInstall +qa
